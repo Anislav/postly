70e6ed3dc75836186e9ff52bcc7d6ff7d741f751
Anislav Atanasov <anislav@gmail.com>
2018-09-25 22:30:29 +0300

    Init components structure

---
+++ src/Comment.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class Comment extends React.Component {
+
+}
+
+export default Comment;
---
+++ src/PostsList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class PostsList extends React.Component {
+
+    render() {
+        return [];
+    }
+}
+
+export default PostsList;
